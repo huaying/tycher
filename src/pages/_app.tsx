f9bc197edca8482c165331fb783fb07a60911145
Huaying <royal3501@gmail.com>
2023-06-02 16:21:21 +0800

    update og url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
             content="Tycher 讓你能夠根據不同主題生成練習題，助你一臂之力。現在就加入我們，讓學習變得更有效率吧！"
           />
           <link rel="icon" href="/favicon.ico" />
-          <meta property="og:image" content="/tycher-og.png" />
+          <meta property="og:image" content="https://tycher.io/tycher-og.png" />
           <meta
             property="og:title"
             content="Tycher來考試, 你的知識, 你的考試"
@@ -33,7 +33,10 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           />
           <meta property="og:url" content="https://tycher.io" />
 
-          <meta property="twitter:image" content="/tycher-og.png" />
+          <meta
+            property="twitter:image"
+            content="https://tycher.io/tycher-og.png"
+          />
           <meta
             property="twitter:title"
             content="Tycher來考試, 你的知識, 你的考試"
